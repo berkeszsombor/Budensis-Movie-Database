0356ae51594ad3e6855fa084b6ead54fabbefae7
berkeszsombor <berkeszsomi@gmail.com>
2020-02-20 20:33:32 +0100

    Created Series.java class

---
+++ bmdb-application/src/Series.java
@@ -0,0 +1,10 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Series extends Media {
+
+	public Series(BigDecimal ID, String Title, String Description, LocalDate Premier) {
+		super(ID, Title, Description, Premier);
+	}
+
+}
