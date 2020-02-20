2d8bb5c9ddaaaab58b81e6609caa602375b6612e
berkeszsombor <berkeszsomi@gmail.com>
2020-02-20 21:02:40 +0100

    Added User field in Review class

--- bmdb-application/src/Review.java
+++ bmdb-application/src/Review.java
@@ -1,8 +1,10 @@
 
 public class Review {
 	public String text;
+	public User user;
 	
-	public Review(String Text) {
+	public Review(String Text, User user) {
 		this.text = Text;
+		this.user = user;
 	}
 }
