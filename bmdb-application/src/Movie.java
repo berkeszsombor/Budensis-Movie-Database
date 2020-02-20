30db8e43ea0313753744826b9c2511dda5de2004
berkeszsombor <berkeszsomi@gmail.com>
2020-02-20 20:34:33 +0100

    Created Movie.java class

---
+++ bmdb-application/src/Movie.java
@@ -0,0 +1,10 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Movie extends Media {
+
+	public Movie(BigDecimal ID, String Title, String Description, LocalDate Premier) {
+		super(ID, Title, Description, Premier);
+	}
+
+}
