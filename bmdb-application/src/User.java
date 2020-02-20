47af60a691b06874651d67ffa784fb328ef03f8b
berkeszsombor <berkeszsomi@gmail.com>
2020-02-20 20:58:49 +0100

    Added List<Review> in User class

--- bmdb-application/src/User.java
+++ bmdb-application/src/User.java
@@ -1,12 +1,23 @@
+import java.util.List;
 
 public class User {
 	public String name;
 	public String email;
 	public String passWord;
 	
+	public List<Review> userReviews;
+	
 	public User(String Name, String Email, String PassWord) {
 		this.name = Name;
 		this.email = Email;
 		this.passWord = PassWord;
+	}
+	
+	public void addReview(Review review) {
+		this.userReviews.add(review);
+	}
+	
+	public List<Review> getReview(){
+		return this.userReviews;
 	}
 }
