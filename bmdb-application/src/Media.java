4bfc203d7747005aaca0fec8c41ce33ef8a6b139
berkeszsombor <berkeszsomi@gmail.com>
2020-02-20 21:08:47 +0100

    Added the cast list in the Media class

--- bmdb-application/src/Media.java
+++ bmdb-application/src/Media.java
@@ -1,10 +1,13 @@
 import java.math.*;
 import java.time.LocalDate;
+import java.util.List;
 public class Media {
 	public BigDecimal id;
 	public String title;
 	public String description;
 	public LocalDate premier;
+	
+	private List<String> cast;
 	
 	public Media(BigDecimal ID, String Title, String Description, LocalDate Premier) {
 		this.id = ID;
@@ -13,4 +16,11 @@ public class Media {
 		this.premier = Premier;
 	}
 
+	public List<String> getCast(){
+		return this.cast;
+	}
+	
+	public void addCast(String actor) {
+		this.cast.add(actor);
+	}
 }
