b6be3dbc328a469637b806036f737e2c9ea989ea
berkeszsombor <berkeszsomi@gmail.com>
2020-02-20 20:46:33 +0100

    Created User.java class

---
+++ bmdb-application/src/User.java
@@ -0,0 +1,12 @@
+
+public class User {
+	public String name;
+	public String email;
+	public String passWord;
+	
+	public User(String Name, String Email, String PassWord) {
+		this.name = Name;
+		this.email = Email;
+		this.passWord = PassWord;
+	}
+}
