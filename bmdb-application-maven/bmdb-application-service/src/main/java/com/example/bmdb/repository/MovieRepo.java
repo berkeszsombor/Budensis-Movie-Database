c88c772898bec360a4118ec8b968de19be30296f
berkeszsombor <berkeszsomi@gmail.com>
2020-04-27 17:35:22 +0200

    Milestone 4 4.0
    Deleted everything and replaced the repo with the new files

--- bmdb-application-maven/bmdb-application-service/src/main/java/com/example/bmdb/repository/MovieRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.bmdb.repository;
-
-import com.example.bmdb.domain.Movie;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MovieRepo extends CrudRepository<Movie, String> {
-}
--- bmdb-application-maven/bmdb-application-service/src/main/java/com/example/bmdb/repository/SeriesRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.bmdb.repository;
-
-import com.example.bmdb.domain.Series;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SeriesRepo extends CrudRepository<Series, String> {
-}
